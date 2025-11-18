cee32fd889f23a8b1ed4f8b37c380f43a10b5c74
Nacxht <zombiecrafter069@gmail.com>
2025-11-18 21:50:29 +0700

    merapikan struktur html login & register

--- login.php
+++ login.php
@@ -1,10 +1,22 @@
 <?php  ?>
-<h1>Login</h1>
-<form method="POST">
-	<p><label id="username">Username : </label><input type="text" name="username"><div class="error-val"></div></p>
-	<p><label id="password">Password : </label><input type="password" name="password"><div class="error-val"></div></p>
-	<p><button type="submit">Login</button></p>
-	<p>Belum memiliki akun? <a href="register.php">Register</a></p>
-	
+<h1 class="title">
+	Login
+</h1>
 
+<form action="login.php" method="post">
+	<div class="input-container">
+		<label for="username">Username</label>
+		<input type="text" name="username" id="username">
+	</div>
+
+	<div class="input-container">
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password">
+	</div>
+
+	<button type="submit">Login</button>
+
+	<p>
+		Belum memiliki akun? <a href="register.php">Register</a>
+	</p>
 </form>
--- register.php
+++ register.php
@@ -1,9 +1,31 @@
-<h1>Register</h1>
-<form method="POST">
-	<p><label id="username">Username : </label><input type="text" name="username"><div class="error-val"></div></p>
-	<p><label id="Email">Email : </label><input type="text" name="username"><div class="error-val"></div></p>
-	<p><label id="password">Password : </label><input type="password" name="password"><div class="error-val"></div></p>
-	<p><label id="konf-password">Konfirmasi Password : </label><input type="konf-password" name="konf-password"><div class="error-val"></div></p>
-	<p><button type="submit">Register</button></p>
-	<p>Sudah memiliki akun? <a href="login.php">Login</a></p>
+<h1 class="title">
+	Register
+</h1>
+
+<form action="register.php" method="post">
+	<div class="input-container">
+		<label for="username">Username</label>
+		<input type="text" name="username" id="username">
+	</div>
+
+	<div class="input-container">
+		<label for="email">Email</label>
+		<input type="text" name="email" id="email">
+	</div>
+
+	<div class="input-container">
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password">
+	</div>
+
+	<div class="input-container">
+		<label for="konfirmasi-password">Konfirmasi Password</label>
+		<input type="password" name="konfirmasi-password" id="konfirmasi-password">
+	</div>
+
+	<button type="submit">Register</button>
+
+	<p>
+		Sudah memiliki akun? <a href="login.php">Login</a>
+	</p>
 </form>
