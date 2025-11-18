47fccc10747d57532207426f101815b2f26fab66
Nacxht <zombiecrafter069@gmail.com>
2025-11-18 19:08:39 +0700

    membuat kerangka file untuk navbar, footer, dan sidebar

---
+++ components/layouts/navbar.php
@@ -0,0 +1,21 @@
+<nav>
+    <div class="branding">
+        <!--  -->
+    </div>
+
+    <div class="navbar-menu">
+        <ul>
+            <li>Homepage</li>
+            <li></li>
+        </ul>
+    </div>
+
+    <div class="login-register">
+        <a href="" class="tbl">Login</a>
+        <a href="" class="tbl">Register</a>
+    </div>
+
+    <!-- <div class="profile">
+        
+    </div> -->
+</nav>
