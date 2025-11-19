fef62ed1455070ef449d459ef0fce59a7e99f317
Nacxht <zombiecrafter069@gmail.com>
2025-11-19 16:00:14 +0700

    me-refaktor struktur kode

--- services/autentikasi_service.php
+++ services/autentikasi_service.php
@@ -7,13 +7,13 @@ session_start();
 function loginService(string $username, string $password, &$errors)
 {
     $stmt = DBH->prepare("
-        SELECT username
+        SELECT username, password, role
         FROM users
         WHERE username=:username
     ");
 
     $stmt->execute([":username" => $username]);
-    $user = $stmt->fetchColumn();
+    $user = $stmt->fetch();
 
     if (!$user) {
         $errors["username"][] = "Username tidak ditemukan";
@@ -31,8 +31,41 @@ function loginService(string $username, string $password, &$errors)
     $_SESSION["role"] = $user["role"];
 
     if ($user["role"] == "admin") {
-        header("location: " . BASE_URL . "/admin/index.php");
+        header("location: " . BASE_URL . "admin/index.php");
     } else {
-        header("location: " . BASE_URL . "/index.php");
+        header("location: " . BASE_URL . "index.php");
+    }
+}
+
+function registerService(string $username, string $email, string $password, array &$errors)
+{
+    try {
+        $sqlCheckUsername = "SELECT * FROM users WHERE username = :username";
+        $stmtCheckUsername = DBH->prepare($sqlCheckUsername);
+
+        $stmtCheckUsername->execute([
+            ':username' => $username
+        ]);
+
+        $user = $stmtCheckUsername->fetch();
+
+        if (!$user) {
+            $hashPassword = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users(username,email,password,role) VALUES(:username,:email,:password,:calon_siswa)";
+            $stmt = DBH->prepare($sql);
+
+            $stmt->execute([
+                ':username' => $username,
+                ':email' => $email,
+                ':password' => $hashPassword,
+                ':calon_siswa' => 'calon_siswa'
+            ]);
+
+            header("location: " . BASE_URL . "index.php");
+        } else {
+            $errors["username"][] = "Username sudah ada!";
+        }
+    } catch (PDOException $e) {
+        echo 'Error : ' . $e->getmessage();
     }
 }
