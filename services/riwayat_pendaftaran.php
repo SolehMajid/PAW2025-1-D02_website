6f3ceb70a094cd5c3e1b00ac75655f1a89d28683
SolehMajid <asolehmajid@gmail.com>
2025-11-20 22:40:11 +0700

    Menambah Validate ukuran file dan file tidak boleh kosong, jurusan belum hehe

--- services/riwayat_pendaftaran.php
+++ services/riwayat_pendaftaran.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
 require_once __DIR__ . '/../db_conn.php';
 
- ?>
+
+function takeRiwayatPendaftaran()
+{
+	try {
+		//
+	} catch (PDOException $e) {
+		echo 'Error : ' . $e->getMessage();
+	}
+}
--- validators/form_pendaftaran_validator.php
+++ validators/form_pendaftaran_validator.php
@@ -11,7 +11,7 @@ function validateNamaLengkap(string $field, array &$errors)
 function validateNik(string $field, array &$errors)
 {
     if (cekFieldKosong($field)) {
-        $errors["nik"][] = "Nik Lengkap Tidak Boleh Kosong";
+        $errors["nik"][] = "Nik Tidak Boleh Kosong";
     }
 
     if (!cekNumeric($field)) {
@@ -26,59 +26,95 @@ function validateNik(string $field, array &$errors)
 function validateJenisKelamin(string $field, array &$errors)
 {
     if (cekFieldKosong($field)) {
-        $errors["jenis_kelamin"][] = "jenis kelamin Lengkap Tidak Boleh Kosong";
+        $errors["jenis_kelamin"][] = "jenis kelamin Tidak Boleh Kosong";
     }
 }
 
 function validateTempatLahir(string $field, array &$errors)
 {
     if (cekFieldKosong($field)) {
-        $errors["tempat_lahir"][] = "Tempat lahir Lengkap Tidak Boleh Kosong";
+        $errors["tempat_lahir"][] = "Tempat lahir Tidak Boleh Kosong";
     }
 }
 
 function validateTanggalLahir(string $field, array &$errors)
 {
     if (cekFieldKosong($field)) {
-        $errors["tanggal_lahir"][] = "Tanggal lahir Lengkap Tidak Boleh Kosong";
+        $errors["tanggal_lahir"][] = "Tanggal lahir Tidak Boleh Kosong";
     }
 }
 
 function validateAsalSekolah(string $field, array &$errors)
 {
     if (cekFieldKosong($field)) {
-        $errors["asal_sekolah"][] = "Asal Sekolah Lengkap Tidak Boleh Kosong";
+        $errors["asal_sekolah"][] = "Asal Sekolah Tidak Boleh Kosong";
     }
 }
 
 function validateAktaKelahiran( $field, array &$errors)
 {
-    //
+    if($field['name'] == ''){
+        $errors['akta_kelahiran'][] = "Tidak Boleh Kosong";
+    }
+
+    if($field['size'] > 1000000 || $field['size'] == 0){
+        $errors['akta_kelahiran'][] = "File Ukuran MAX 1MB";
+    }
 }
 
 function validateKartuKeluarga( $field, array &$errors)
 {
-    //
+    if($field['name'] == ''){
+        $errors['kartu_keluarga'][] = "Tidak Boleh Kosong";
+    }
+
+    if($field['size'] > 1000000 || $field['size'] == 0){
+        $errors['kartu_keluarga'][] = "File Ukuran MAX 1MB";
+    }
 }
 
 function validateRapor( $field, array &$errors)
 {
-    //
+    if($field['name'] == ''){
+        $errors['rapor'][] = "Tidak Boleh Kosong";
+    }
+
+    if($field['size'] > 1000000 || $field['size'] == 0){
+        $errors['rapor'][] = "File Ukuran MAX 1MB";
+    }
 }
 
 function validateSuratKeteranganLulus( $field, array &$errors)
 {
-    //
+    if($field['name'] == ''){
+        $errors['surat_keterangan_lulus'][] = "Tidak Boleh Kosong";
+    }
+
+    if($field['size'] > 1000000 || $field['size'] == 0){
+        $errors['surat_keterangan_lulus'][] = "File Ukuran MAX 1MB";
+    }
 }
 
 function validateSuratKesehatan( $field, array &$errors)
 {
-    //
+    if($field['name'] == ''){
+        $errors['surat_kesehatan'][] = "Tidak Boleh Kosong";
+    }
+
+    if($field['size'] > 1000000 || $field['size'] == 0){
+        $errors['surat_kesehatan'][] = "File Ukuran MAX 1MB";
+    }
 }
 
 function validatePasfoto( $field, array &$errors)
 {
-    //
+    if($field['name'] == ''){
+        $errors['pasfoto'][] = "Tidak Boleh Kosong";
+    }
+
+    if($field['size'] > 1000000 || $field['size'] == 0){
+        $errors['pasfoto'][] = "File Ukuran MAX 1MB";
+    }
 }
 
 function validatePersetujuanAsrama(string $field, array &$errors)
