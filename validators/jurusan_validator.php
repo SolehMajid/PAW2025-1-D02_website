6224d5b2b3f98793cc9985732b423e4033989965
Nacxht <zombiecrafter069@gmail.com>
2025-11-27 02:01:22 +0700

    menambahkan validator untuk jurusan

---
+++ validators/jurusan_validator.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . "/../config.php";
+require_once __DIR__ . "/../db_conn.php";
+require_once __DIR__ . "/base_validator.php";
+
+function validateJurusanName(string $field, array &$errors)
+{
+    // 
+}
+
+function validateJurusanDescription(string $field, array &$errors)
+{
+    // 
+}
