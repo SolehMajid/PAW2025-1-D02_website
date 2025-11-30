1c5d267c621a3d6a977fe16703027a747c484d97
Nacxht <zombiecrafter069@gmail.com>
2025-11-30 13:37:18 +0700

    menghapus halaman yang tidak diperlukan

--- auth_middleware/after_login_middleware.php
+++ auth_middleware/after_login_middleware.php
@@ -7,6 +7,6 @@ if (isset($_SESSION["username"]) && isset($_SESSION["role"])) {
     if ($_SESSION["role"] == "admin") {
         header("location: " . BASE_URL . "admin/");
     } else {
-        header("location: " . BASE_URL . "calon_siswa/");
+        header("location: " . BASE_URL);
     }
 }
